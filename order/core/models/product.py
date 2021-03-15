c641a453f0c04dd9092202c14d875b7d134f1b06
Luis <devcorujam@gmail.com>
2021-03-15 19:43:43 -0300

    ✨ Add product model

---
+++ order/core/models/product.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Product(models.Model):
+    class CurrencyChoices(models.TextChoices):
+        REAL = 'R$'
+        DOLAR = "$"
+        EURO = "€"
+
+    name = models.CharField(max_length=150)
+    currency = models.CharField(max_length=5, choices=CurrencyChoices.choices, default=CurrencyChoices.REAL)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    multiplier = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} - {self.currency} {self.price}'
