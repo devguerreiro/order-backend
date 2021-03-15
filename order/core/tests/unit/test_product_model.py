b5645015dbb94417fdd488b66b34659d9b13d217
Luis <devcorujam@gmail.com>
2021-03-15 19:44:09 -0300

    ✅ Add product model test

---
+++ order/core/tests/unit/test_product_model.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from order.core.models import Product
+
+class ProductModelTestCase(TestCase):
+    def test_should_has_attributes(self):
+        self.assertIs(hasattr(Product, "name"), True)
+        self.assertIs(hasattr(Product, "currency"), True)
+        self.assertIs(hasattr(Product, "price"), True)
+        self.assertIs(hasattr(Product, "multiplier"), True)
+    
+    def test_str_should_return_product_name_and_price_with_currency(self):
+        product = Product(name="Notebook", currency=Product.CurrencyChoices.REAL, price=2_599_00)
+        expected = f'{product.name} - {product.currency} {product.price}'
+        self.assertEqual(expected, str(product))
